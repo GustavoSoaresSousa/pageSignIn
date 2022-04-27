2906ded2357bac59c0faed5e5e1560ebbadaf9ae
Gustavo Soares <gustavosoaresdev10@gmail.com>
2022-04-26 21:38:12 -0300

    add  boostrap in financeIndex

--- frontend/src/components/financesComponents/FinanceIndex.js
+++ frontend/src/components/financesComponents/FinanceIndex.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './Finance.css';
 import api from '../../services/api'
 import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import {Chart, GoogleDataTableColumnRoleType} from 'react-google-charts'
+  import _ from 'lodash';
 
 const dataFinanceEdited = {
   name: '',
@@ -12,7 +14,6 @@ const dataFinanceEdited = {
 export function FinanceIndex() {
   const [allFinances, setAllFinances] = useState([]);
   const [financeToEdit, setFinanceToEdit] = useState(dataFinanceEdited);
-  const [idToEdit, setIdToEdit] = useState('');
 
   useEffect(() => {
     async function getAllFinances() {
@@ -21,6 +22,10 @@ export function FinanceIndex() {
     }
     getAllFinances();
   }, [allFinances]);
+
+  async function deleteFinance(id) {
+    const response = await api.delete(`/finances/${id}`);
+  }
 
   function handleInput(e) {
     const { value, name } = e.target;
@@ -31,25 +36,20 @@ export function FinanceIndex() {
     })
   }
 
-  async function deleteFinance(id) {
-    const response = await api.delete(`/finances/${id}`);
-  }
-
-  async function editFinance(data) {
-    const response = await api.put(`/finances/${data._id}`, {
+  async function editFinance(id) {
+      const response = await api.put(`/finances/${id}`, {
       name: financeToEdit.name,
       description: financeToEdit.description,
       value: financeToEdit.value
     });
-    setFinanceToEdit(dataFinanceEdited)
+    setFinanceToEdit(dataFinanceEdited);
   }
 
   return (
     <div className="div-finance-container-extra">
-      <div className="div-finance-content">
-        <div className="info-finance">
-          <table>
-            <thead>
+      <div className="div-finance-content col-md-12">
+          <table className="table table-boderless table-striped">
+            <thead className="thead-ligth">
               <tr>
                 <td>Name</td>
                 <td>Description</td>
@@ -60,47 +60,22 @@ export function FinanceIndex() {
 
             <tbody>
               {
-                Object.keys(allFinances).map(data => {
-                  return <><tr className="tr-datas" key={allFinances[data].id}>
-                    <td>{allFinances[data].name}</td>
-                    <td>{allFinances[data].description}</td>
-                    <td>{allFinances[data].value}</td>
-                    <td className="td-functions">
-                      {/* <a type="button"data-toggle="modal" data-target="#exampleModal">
+                allFinances.map( data => {
+                  return <tr key={data._id}>
+                    <td>{data.name}</td>
+                    <td>{data.description}</td>
+                    <td>{data.value}</td>
+                    <td className='td-functions'>
+                      <a onClick={() => deleteFinance(data._id)}><AiOutlineDelete /></a> 
+                      <a>
                         <AiOutlineEdit />
                       </a>
-                      <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <form autoComplete="off">
-                        <div className="modal-dialog" role="document">
-                          <div className="modal-content">
-                            <div className="modal-header">
-                              <h5 className="modal-title" id="exampleModalLabel">Update finance</h5>
-                              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div className="modal-body">
-                              <input type="text" placeholder="Name" value={allFinances[data].name} name="name" onChange={handleInput} />
-                              <input type="text" placeholder="Description" value={allFinances[data].description} name="description" onChange={handleInput} />
-                              <input type="text" placeholder="Value"  value={allFinances[data].value} name="value" onChange={handleInput} />
-                            </div>
-                            <div className="modal-footer">
-                              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                              <button onClick={() => {editFinance(allFinances[data].id)}} type="submit" className="btn btn-primary">Update finances</button>
-                            </div>
-                          </div>
-                        </div>
-                        </form>
-
-                      </div>*/}
-                      <a onClick={() => deleteFinance(allFinances[data]._id)}><AiOutlineDelete /></a> 
                     </td>
-                  </tr></>
+                  </tr>
                 })
               }
             </tbody>
           </table>
-        </div>
       </div>
     </div>
 
